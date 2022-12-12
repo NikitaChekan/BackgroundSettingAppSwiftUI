3fa921ea0e73967df5c24cf5d69b428bfdd3167f
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-12 14:52:16 +0300

    Initial Commit

---
+++ BackgroundSettingAppSwiftUI/BackgroundSettingAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundSettingAppSwiftUIApp.swift
+//  BackgroundSettingAppSwiftUI
+//
+//  Created by jopootrivatel on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundSettingAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
